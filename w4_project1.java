305e8a476a61e7a99e2dbeb6e1d0b5b9498aeb18
JorVor <129814086+Jorvor37@users.noreply.github.com>
2025-05-16 00:18:36 +0700

    Add files via upload

---
+++ w4_project1.java
@@ -0,0 +1,21 @@
+package Project1_6680081;
+
+/*
+    @author Kongphop Kayoonvihcien      ID: 6680081
+            Wasupon Wiriyakitanan       ID: 6680646
+            Phattarada Limsuchaiwat     ID: 6680684
+            
+*/
+
+import java.io.*;
+import java.util.*;
+
+public class w4_project1 {
+    public static void main(String[] args){
+        String path = "src/main/java/Project1_6680081/"; 
+        String inBookings   = path + "bookings.txt";    //read Bookings
+        String inDiscounts  = path + "discounts.txt";   //read Discounts
+        String inItems      = path + "items.txt";       //read Items
+        System.out.println("Read input from "+path+"countries.txt");
+    }
+}
